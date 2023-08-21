c3b859065494036279882220de68548bc3212aab
JosephPrichard <58538077+JosephPrichard@users.noreply.github.com>
2023-08-21 11:33:48 -0500

    Added drawing functionality to canvas

--- frontend/src/components/canvas/Canvas.tsx
+++ frontend/src/components/canvas/Canvas.tsx
@@ -5,17 +5,34 @@ const COLORS = [
     "white", "grey", "red", "orange", "yellow", "lime", 
     "darkgreen", "cyan", "blue", "purple", "pink", "brown"
 ];
-const SIZES = [2, 4, 8, 16, 32];
+const SIZES = [4, 6, 8, 12, 16];
 
 const Canvas = () => {
     const [canvasRef, setCanvasRef] = createSignal<HTMLCanvasElement | null>(null);
-    const [color, setColor] = createSignal(0);
-    const [size, setSize] = createSignal(0);
+    const [colorIndex, setColorIndex] = createSignal(0);
+    const [sizeIndex, setSizeIndex] = createSignal(0);
+    const [isDrawing, setIsDrawing] = createSignal(false);
 
     const getCanvas = () => {
         const canvas = canvasRef()!;
         const ctx = canvas.getContext("2d")!;
         return { canvas, ctx }
+    };
+
+    const drawCircle = (e: MouseEvent) => {
+        const { canvas, ctx } = getCanvas();
+        const rect = canvas.getBoundingClientRect();
+        // calcualte the scale and location to draw the circle
+        const scaleX = canvas.width / rect.width;
+        const scaleY = canvas.height / rect.height;
+        const x = scaleX * (e.clientX - rect.left);
+        const y = scaleY * (e.clientY - rect.top);
+        // draw the colored circle at the location on the canvas
+        ctx.fillStyle = COLORS[colorIndex()];
+        ctx.beginPath();
+        ctx.arc(x, y, SIZES[sizeIndex()], 0, 2 * Math.PI);
+        ctx.fill();
+        console.log("Draw circle", x, y, ctx.fillStyle)
     };
 
     onMount(() => {
@@ -26,30 +43,56 @@ const Canvas = () => {
 
     return (
         <div>
-            <canvas ref={setCanvasRef} class="Canvas" />
-            <div class="Paint">
-                <div>
-                    <Index each={COLORS}>
-                        {(color, i) => (
-                            <div 
-                                class="SmallTile" 
-                                style={{ "background-color": color() }} 
-                                onclick={() => setColor(i)}
-                            />
-                        )}
-                    </Index>
-                </div>
-                <div>
-                    <Index each={SIZES}>
-                        {(size, i) => (
-                            <div 
-                                class="LargeTile"
-                                onclick={() => setSize(i)}
-                            />
-                        )}
-                    </Index>
-                    <div class="LargeTile" />
-                </div>
+            <canvas 
+                ref={setCanvasRef} 
+                class="Canvas"
+                width="700"
+                height="450"
+                onmousemove={(e) => {
+                    if (isDrawing()) {
+                        drawCircle(e);
+                    }
+                }}
+                onmousedown={(e) => {
+                    drawCircle(e);
+                    setIsDrawing(true);
+                }}
+                onmouseup={() => setIsDrawing(false)}
+            />
+            <Easel setColor={setColorIndex} setSize={setSizeIndex} />
+        </div>
+    );
+}
+
+interface EaselProps {
+    setColor: (c: number) => void;
+    setSize: (s: number) => void;
+}
+
+const Easel = ({ setColor, setSize }: EaselProps) => {
+    return (
+        <div class="Paint">
+            <div>
+                <Index each={COLORS}>
+                    {(color, i) => (
+                        <div 
+                            class="SmallTile" 
+                            style={{ "background-color": color() }} 
+                            onclick={() => setColor(i)}
+                        />
+                    )}
+                </Index>
+            </div>
+            <div>
+                <Index each={SIZES}>
+                    {(size, i) => (
+                        <div 
+                            class="LargeTile"
+                            onclick={() => setSize(i)}
+                        />
+                    )}
+                </Index>
+                <div class="LargeTile" />
             </div>
         </div>
     );
